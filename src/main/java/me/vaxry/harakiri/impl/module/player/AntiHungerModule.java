598528f482b0357d3d525dba67780f8a0398f9e6
Vaxry <ralfik.q@wp.pl>
2021-01-29 19:31:46 +0100

    added antihunger

---
+++ src/main/java/me/vaxry/harakiri/impl/module/player/AntiHungerModule.java
@@ -0,0 +1,41 @@
+package me.vaxry.harakiri.impl.module.player;
+
+import me.vaxry.harakiri.api.event.network.EventSendPacket;
+import me.vaxry.harakiri.api.module.Module;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.CPacketEntityAction;
+import net.minecraft.network.play.client.CPacketPlayer;
+import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
+
+public class AntiHungerModule extends Module {
+
+    public AntiHungerModule() {
+        super("AntiHunger", new String[]{"AntiHunger", "AH"}, "Makes you lose less hunger", "NONE", -1, ModuleType.PLAYER);
+    }
+
+    @Listener
+    public void onPacketSend(EventSendPacket event){
+        final Minecraft mc = Minecraft.getMinecraft();
+        if(event.getPacket() instanceof CPacketPlayer && mc.player.onGround && !mc.player.isElytraFlying()){
+            final CPacketPlayer packet = (CPacketPlayer)event.getPacket();
+
+            if (mc.player.fallDistance > 0 || mc.playerController.isHittingBlock)
+            {
+                packet.onGround = true;
+            }
+            else
+            {
+                packet.onGround = false;
+            }
+        }
+
+        if (event.getPacket() instanceof CPacketEntityAction && mc.player.isSprinting())
+        {
+            final CPacketEntityAction l_Packet = (CPacketEntityAction) event.getPacket();
+            if (l_Packet.getAction() == CPacketEntityAction.Action.START_SPRINTING || l_Packet.getAction() == CPacketEntityAction.Action.STOP_SPRINTING)
+            {
+                event.setCanceled(true);
+            }
+        }
+    }
+}
