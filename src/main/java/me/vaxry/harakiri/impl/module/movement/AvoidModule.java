af33e6bcd0b85f84afe4cc4c77ef8e8373f203af
Vaxry <ralfik.q@wp.pl>
2021-03-15 09:21:25 +0100

    dont avoid in freecam

--- src/main/java/me/vaxry/harakiri/impl/module/movement/AvoidModule.java
+++ src/main/java/me/vaxry/harakiri/impl/module/movement/AvoidModule.java
@@ -1,9 +1,12 @@
 package me.vaxry.harakiri.impl.module.movement;
 
+import me.vaxry.harakiri.Harakiri;
 import me.vaxry.harakiri.framework.event.player.EventMove;
 import me.vaxry.harakiri.framework.module.Module;
 import me.vaxry.harakiri.framework.value.Value;
+import me.vaxry.harakiri.impl.module.player.FreeCamModule;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.util.math.BlockPos;
 import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
 
@@ -18,12 +21,13 @@ public class AvoidModule extends Module {
 
     @Listener
     public void move(EventMove event) {
-        fixMovevemt(event);
+        if(!Harakiri.get().getModuleManager().find(FreeCamModule.class).isEnabled())
+            fixMovevemt(event, Minecraft.getMinecraft().player);
 
         // ezd
     }
 
-    public void fixMovevemt(EventMove event){
+    public void fixMovevemt(EventMove event, EntityPlayerSP player){
         Minecraft mc = Minecraft.getMinecraft();
 
         // Check if we are colliding on 4 axes.
@@ -31,13 +35,13 @@ public class AvoidModule extends Module {
         BlockPos[] surround = new BlockPos[4];
 
         // +X
-        surround[0] = new BlockPos(mc.player.getPosition().getX() + 1, mc.player.getPosition().getY(), mc.player.getPosition().getZ());
+        surround[0] = new BlockPos(player.getPosition().getX() + 1, player.getPosition().getY(), player.getPosition().getZ());
         // -X
-        surround[1] = new BlockPos(mc.player.getPosition().getX() - 1, mc.player.getPosition().getY(), mc.player.getPosition().getZ());
+        surround[1] = new BlockPos(player.getPosition().getX() - 1, player.getPosition().getY(), player.getPosition().getZ());
         // +Z
-        surround[2] = new BlockPos(mc.player.getPosition().getX(), mc.player.getPosition().getY(), mc.player.getPosition().getZ() + 1);
+        surround[2] = new BlockPos(player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ() + 1);
         // -Z
-        surround[3] = new BlockPos(mc.player.getPosition().getX(), mc.player.getPosition().getY(), mc.player.getPosition().getZ() - 1);
+        surround[3] = new BlockPos(player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ() - 1);
 
         // Check all
         if(isBlockUnloaded(surround[0])){
