d68c61ea6d7f1a29b28bc44aa8b2b0cb1edc128a
Vaxry <ralfik.q@wp.pl>
2021-03-19 15:35:46 +0100

    fix render 3d box

--- src/main/java/me/vaxry/harakiri/framework/lua/api/RenderAPI.java
+++ src/main/java/me/vaxry/harakiri/framework/lua/api/RenderAPI.java
@@ -2,6 +2,8 @@ package me.vaxry.harakiri.framework.lua.api;
 
 import me.vaxry.harakiri.Harakiri;
 import me.vaxry.harakiri.framework.util.RenderUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.util.math.AxisAlignedBB;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
@@ -78,6 +80,9 @@ public class RenderAPI extends TwoArgFunction {
 
     protected static class render3DBox extends VarArgFunction {
         public Varargs invoke(Varargs args) {
+            RenderUtil.begin3D();
+
+            final Minecraft mc = Minecraft.getMinecraft();
             int x = args.arg(1).checkint();
             int y = args.arg(2).checkint();
             int z = args.arg(3).checkint();
@@ -86,14 +91,32 @@ public class RenderAPI extends TwoArgFunction {
             int z1 = args.arg(6).checkint();
             double thick = args.arg(7).checkdouble();
             int col = args.arg(8).checkint();
-            AxisAlignedBB bb = new AxisAlignedBB(x,y,z,x1,y1,z1);
-            RenderUtil.drawBoundingBox(bb, (float)thick, col);
+
+            float alpha = (float) (col >> 24 & 255) / 255.0F;
+            float red = (float) (col >> 16 & 255) / 255.0F;
+            float green = (float) (col >> 8 & 255) / 255.0F;
+            float blue = (float) (col & 255) / 255.0F;
+
+            final AxisAlignedBB bb = new AxisAlignedBB(
+                    x - mc.getRenderManager().viewerPosX,
+                    y - mc.getRenderManager().viewerPosY,
+                    z - mc.getRenderManager().viewerPosZ,
+                    x1 - mc.getRenderManager().viewerPosX,
+                    y1 - mc.getRenderManager().viewerPosY,
+                    z1 - mc.getRenderManager().viewerPosZ);
+            
+            RenderGlobal.drawBoundingBox(bb.minX, bb.minY, bb.minZ, bb.maxX, bb.maxY, bb.maxZ, red, green, blue, alpha);
+
+            RenderUtil.end3D();
+
             return LuaValue.valueOf(1);
         }
     }
 
     protected static class render3DLine extends VarArgFunction {
         public Varargs invoke(Varargs args) {
+            RenderUtil.begin3D();
+
             int x = args.arg(1).checkint();
             int y = args.arg(2).checkint();
             int z = args.arg(3).checkint();
@@ -103,6 +126,8 @@ public class RenderAPI extends TwoArgFunction {
             double thick = args.arg(7).checkdouble();
             int col = args.arg(8).checkint();
             RenderUtil.drawLine3D(x,y,z,x1,y1,z1,(float)thick,col);
+
+            RenderUtil.end3D();
             return LuaValue.valueOf(1);
         }
     }
