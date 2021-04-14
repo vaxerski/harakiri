cdfe83f26a076e29863ac5419a8aa4700c81a62e
Vaxry <ralfik.q@wp.pl>
2021-04-14 09:27:35 +0200

    better antiafk

--- src/main/java/me/vaxry/harakiri/impl/module/misc/NoAfkModule.java
+++ src/main/java/me/vaxry/harakiri/impl/module/misc/NoAfkModule.java
@@ -7,24 +7,39 @@ import me.vaxry.harakiri.framework.event.player.EventUpdateWalkingPlayer;
 import me.vaxry.harakiri.framework.Module;
 import me.vaxry.harakiri.framework.task.rotation.RotationTask;
 import me.vaxry.harakiri.framework.Value;
+import me.vaxry.harakiri.framework.util.MathUtil;
+import me.vaxry.harakiri.framework.util.Timer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.util.EnumHand;
 import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
 
 public final class NoAfkModule extends Module {
 
-    public final Value<Integer> yawOffset = new Value<Integer>("Yaw", new String[]{"yaw", "y"}, "The yaw to alternate each tick.", 1, 0, 180, 1);
+    public final Value<Boolean> yawOffset = new Value<Boolean>("Yaw", new String[]{"yaw", "y"}, "Moves the yaw.", false);
+    public final Value<Boolean> jump = new Value<Boolean>("Jump", new String[]{"jump", "j"}, "Jumps.", false);
+    public final Value<Boolean> punch = new Value<Boolean>("Punch", new String[]{"punch", "p"}, "Punches.", false);
+    public final Value<Integer> delay = new Value<Integer>("Delay", new String[]{"delay", "d"}, "Delay between actions (seconds).", 1, 1, 5, 1);
 
-    private final RotationTask rotationTask = new RotationTask("NoAFKTask", 1); /* 1 == low priority */
+    private final Timer timer = new Timer();
+
+    private final RotationTask rotationTask = new RotationTask("noafk", 2);
 
     public NoAfkModule() {
         super("AntiAFK", new String[]{"AntiAFK"}, "Prevents you from being kicked while AFK.", "NONE", -1, ModuleType.MISC);
+        timer.reset();
+        Harakiri.get().getRotationManager().finishTask(this.rotationTask);
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        timer.reset();
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
-        Harakiri.get().getRotationManager().finishTask(this.rotationTask);
     }
 
     @Listener
@@ -33,34 +48,23 @@ public final class NoAfkModule extends Module {
         if (mc.player == null || mc.world == null)
             return;
 
-        switch (event.getStage()) {
-            case PRE:
-                float yaw = mc.player.rotationYaw;
+        if(timer.passed(this.delay.getValue()*1000) && event.getStage() == EventStageable.EventStage.PRE){
+
+            if(yawOffset.getValue()){
+                float yaw = (float) Math.random() * 360F - 180F;
                 float pitch = mc.player.rotationPitch;
-                yaw += (this.yawOffset.getValue() * Math.sin(mc.player.ticksExisted / Math.PI));
-
-                Harakiri.get().getRotationManager().startTask(this.rotationTask);
-                if (this.rotationTask.isOnline()) {
-                    Harakiri.get().getRotationManager().setPlayerRotations(yaw, pitch);
-                }
-                break;
-            case POST:
-                if (this.rotationTask.isOnline())
-                    Harakiri.get().getRotationManager().finishTask(this.rotationTask);
-                break;
-        }
-    }
-
-    @Listener
-    public void sendPacket(EventSendPacket event) {
-        if (event.getStage() == EventStageable.EventStage.PRE) {
-            if (event.getPacket() instanceof CPacketPlayer.Rotation) {
-                if (Minecraft.getMinecraft().player.getRidingEntity() != null) {
-                    final CPacketPlayer.Rotation packet = (CPacketPlayer.Rotation) event.getPacket();
-                    packet.yaw += (this.yawOffset.getValue() * Math.sin(Minecraft.getMinecraft().player.ticksExisted / Math.PI));
-                }
+                Harakiri.get().getRotationManager().setPlayerRotations(yaw, pitch);
             }
+
+            if(jump.getValue())
+                mc.player.jump();
+
+            if(punch.getValue())
+                mc.player.swingArm(EnumHand.MAIN_HAND);
+
+            timer.reset();
         }
+
     }
 
 }
