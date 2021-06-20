166f125fc9493c70015fa83d59cc2b926ef53e08
vaxerski <43317083+vaxerski@users.noreply.github.com>
2021-06-20 14:34:45 +0200

    macro bariton

--- src/main/java/me/vaxry/harakiri/impl/module/hidden/MacroModule.java
+++ src/main/java/me/vaxry/harakiri/impl/module/hidden/MacroModule.java
@@ -24,7 +24,7 @@ public final class MacroModule extends Module {
                 final String[] split = macro.getMacro().split(";");
 
                 for (String s : split) {
-                    s = "." + s;
+                    if(s.indexOf("#") == 0) s = "." + s;
                     Minecraft.getMinecraft().player.sendChatMessage(s);
                 }
             }
