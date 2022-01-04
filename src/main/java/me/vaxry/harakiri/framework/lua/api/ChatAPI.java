e5bb4adff0cc60486e165000ca5f86dfc92902b3
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-01-04 11:28:19 +0100

    added chat raw

--- src/main/java/me/vaxry/harakiri/framework/lua/api/ChatAPI.java
+++ src/main/java/me/vaxry/harakiri/framework/lua/api/ChatAPI.java
@@ -1,6 +1,8 @@
 package me.vaxry.harakiri.framework.lua.api;
 
 import me.vaxry.harakiri.Harakiri;
+import net.minecraft.client.Minecraft;
+
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
@@ -15,14 +17,22 @@ public class ChatAPI extends TwoArgFunction {
     public LuaValue call(LuaValue modname, LuaValue env) {
         LuaTable chat = new LuaTable(0,30);
         chat.set( "log", new logchat() );
+        chat.set( "raw", new raw() );
         env.set( "chat", chat );
         env.get("package").get("loaded").set("chat", chat);
         return chat;
     }
 
     protected static class logchat extends OneArgFunction {
-        public LuaValue call(LuaValue message){
+        public LuaValue call(LuaValue message) {
             Harakiri.get().logChat(message.toString());
+            return LuaValue.valueOf(1);
+        }
+    }
+
+    protected static class raw extends OneArgFunction {
+        public LuaValue call(LuaValue message) {
+            Minecraft.getMinecraft().player.sendChatMessage(message.toString());
             return LuaValue.valueOf(1);
         }
     }
